b14ac0626e28d8cdb7fa8dbc7b7e5fd4fa254467
alexeylebedbp <alexeylebedbp@github.com>
2024-12-10 14:31:23 +0300

    add mailru file

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.sendFile("/home/ubuntu/nalabe_landing/index.html")
 })
 
+app.get('/mailru-domain2j6oXOkKaSZOtq2A.html', (req, res) => {
+    res.sendFile("/home/ubuntu/nalabe_landing/mailru-domain2j6oXOkKaSZOtq2A.html")
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
